1ddead1e58edfbfadd536528dea43fcab7986858
yoyok.triwicaksono <yoyok.thewicaksono@gmail.com>
2016-09-14 14:20:16 +0700

    # add mongooseapp6

---
+++ mongooseapp6/config/env/development.js
@@ -0,0 +1,7 @@
+// Configuration for development environment
+
+module.exports = {
+	// Development configuration options
+	db: 'mongodb://localhost/mean',
+	sessionSecret: 'developmentSessionSecret'
+};
