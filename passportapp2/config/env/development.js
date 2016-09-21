2a3fee32d161cb5237aeefcc8dd5d21e25e199dc
yoyok.triwicaksono <yoyok.thewicaksono@gmail.com>
2016-09-21 15:50:05 +0700

    add note for google client ID

--- passportapp2/config/env/development.js
+++ passportapp2/config/env/development.js
@@ -4,6 +4,7 @@ module.exports = {
 	// Development configuration options
 	db: 'mongodb://localhost/mean',
 	sessionSecret: 'developmentSessionSecret',
+	// Change this as yours. This is for testing purpose !!!
 	google: {
 		clientID: '256816843091-chbk8tlu5n5bh4eejvtutitvner9st5b.apps.googleusercontent.com',
 		clientSecret: 'SJ9vhR9M2Y6SEOolEPAhaQ-B',
